07e8a4212ab5aa1344d424a5fb07a46bc1fd8040
Vinh Huynh <vinhhv@live.com>
2023-06-20 16:43:47 +0700

    Modify main.py

--- Readme.md
+++ Readme.md
@@ -1,2 +1,6 @@
 Tool to download youtube video or mp3
 Note: Please copy ffmpeg.exe to virtual environment bin folder
+
+
+Make .exe file
+pyinstaller --noconfirm --onefile --windowed  "C:/!Vinh_DBS/MyProject/youtubeDownloader/main.py"
--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import ffmpeg
 BITRATE = "192k"
 
 
+# https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS2
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
 def ask_save_location():
     folder_path = filedialog.askdirectory()
     return folder_path
@@ -17,15 +29,8 @@ def getVideo():
     link = yt_link.get()
     yt = YouTube(
         link,
-        # use_oauth=True,
-        # allow_oauth_cache=True,
     )
     save_location = ask_save_location()
-    # print("Title:", yt.title)
-    # print("Author:", yt.author)
-    # print("Published date:", yt.publish_date.strftime("%Y-%m-%d"))
-    # print("Number of views:", yt.views)
-    # print("Length of video:", yt.length, "seconds")
     yd = yt.streams.get_highest_resolution()
     yd.download(save_location)
     return save_location
@@ -64,35 +69,39 @@ def getAAC():
 
 window = Tk()  # Instantiate an instance of a window.
 
-window.geometry("420x420")  # Size 420px x 420px
+# window.geometry("420x70")  # Size (WxH)
 window.title("Youtube Downloader")
 
-icon = PhotoImage(file="images/yt_icon.png")
+icon = PhotoImage(file=resource_path("images/yt_icon.png"))
 window.iconphoto(True, icon)
 
 # change background color
 window.config(
-    background="#777f85"
+    background="#384531"
 )  # or you can search google with "hex color picker" keyword
 
 # Code in here ....................
 # Create a entry box to user paste the youtube link
 # Link test https://www.youtube.com/watch?v=_8vekzCF04Q&pp=ygUNdmFuIG1haSBodW9uZw%3D%3D
 
+yt_label = Label(window, text="Youtube Link", border=5)
+yt_label.grid(row=0, column=0, padx=5, pady=10)
+
 yt_link = Entry(
     window,
     width=50,
+    border=5,
 )
-yt_link.pack()
+yt_link.grid(row=0, column=1, padx=3)
 
-yt_linkVideo = Button(window, text="Get MP4 Video File", command=getVideo)
-yt_linkVideo.pack()
+yt_linkVideo = Button(window, text="Get MP4 Video File", command=getVideo, border=5)
+yt_linkVideo.grid(row=1, column=0, columnspan=2, pady=2)
 
-yt_linkMP3 = Button(window, text="Get MP3 Audio", command=getMP3)
-yt_linkMP3.pack()
+yt_linkMP3 = Button(window, text="Get MP3 Audio", command=getMP3, border=5)
+yt_linkMP3.grid(row=2, column=0, columnspan=2, pady=2)
 
-yt_linkAAC = Button(window, text="Get AAC Audio", command=getAAC)
-yt_linkAAC.pack()
+yt_linkAAC = Button(window, text="Get AAC Audio", command=getAAC, border=5)
+yt_linkAAC.grid(row=3, column=0, columnspan=2, pady=2)
 
 
 window.mainloop()  # Place window o computer screen, listen for events.
